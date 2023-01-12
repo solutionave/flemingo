0ddf03c07b139f9823ab2fda3a0f89cabc006a93
Waleed <waleedmukhtar1999@gmail.com>
2023-01-12 19:33:39 +0500

    navbar approx done

--- layouts/navbar/navbar.tsx
+++ layouts/navbar/navbar.tsx
@@ -1,59 +1,51 @@
 import React from "react";
 import styles from "./navbar.module.css";
 import Link from "next/link";
-import {
-  FaYoutube,
-  FaTwitter,
-  FaInstagram,
-  FaFacebook,
-  FaTiktok,
-} from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav className="navbar px-5 pt-3 navbar-expand-md ">
-       <a className="navbar-brand navLogo" href="#">
-        <div className={styles.brand}>
-          <Link className={styles.navLink} href={"/"}>
+    <div>
+      <div className="navbar px-5 pt-3 navbar-expand-md">
+        <div className="navbar-brand">
+          <Link className="nav-link" href={"/"}>
             Flamingo
           </Link>
         </div>
-      </a> 
-      <button
-        className="navbar-toggler navbar-light"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ">
-          <li className={styles.navbarItem}>
-            <Link className={styles.navLink} href={"/features"}>
+        <button
+          className="navbar-toggler navbar-light"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+          <div className="nav-item">
+            <Link className="nav-link" href={"/features"}>
               Features
             </Link>
-          </li>
-          <li className={styles.navbarItem}>
-            <Link className={styles.navLink} href={"/pricing"}>
+          </div>
+          <div className="nav-item">
+            <Link className="nav-link" href={"/pricing"}>
               Pricing
             </Link>
-          </li>
-          <li className={styles.navbarItem}>
-            <Link className={styles.navLink} href={"/testimonials"}>
+          </div>
+          <div className="nav-item">
+            <Link className="nav-link" href={"/testimonials"}>
               Testimonials
             </Link>
-          </li>
-          {/* <li className={styles.navbarItem}> */}
-            {/* <button className="Btn">Login</button> */}
-          {/* </li> */}
-        </ul>
+          </div>
+          <div className="nav-item">
+            <button className="Btn">Login</button>
+          </div>
+          </ul>
+        </div>
       </div>
-    </nav>
+    </div>
   );
 };
 
